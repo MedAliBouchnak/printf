c82c7767e49b07123b79b84e78d0c49dadb6066b
Haythem <heythem.94@gmail.com>
2023-10-17 23:01:04 +0000

    Fxing errors

--- functions.c
+++ functions.c
@@ -12,7 +12,8 @@
  * @size: Size specifier
  * Return: Number of chars printed
  */
-int print_char(va_list types, char buffer[], int flags, int width, int precision, int size)
+int print_char(va_list types, char buffer[], int flags, 
+		int width, int precision, int size)
 {
 	char c = va_arg(types, int);
 
@@ -29,7 +30,8 @@ int print_char(va_list types, char buffer[], int flags, int width, int precision
  * @size: Size specifier
  * Return: Number of chars printed
  */
-int print_string(va_list types, char buffer[], int flags, int width, int precision, int size)
+int print_string(va_list types, char buffer[], int flags, int width,
+	       	int precision, int size)
 {
 	int length = 0, i;
 	char *str = va_arg(types, char *);
@@ -83,7 +85,8 @@ int print_string(va_list types, char buffer[], int flags, int width, int precisi
  * @size: Size specifier
  * Return: Number of chars printed
  */
-int print_percent(va_list types, char buffer[], int flags, int width, int precision, int size)
+int print_percent(va_list types, char buffer[], int flags, 
+		int width, int precision, int size)
 {
 	UNUSED(types);
 	UNUSED(buffer);
@@ -105,7 +108,8 @@ int print_percent(va_list types, char buffer[], int flags, int width, int precis
  * @size: Size specifier
  * Return: Number of chars printed
  */
-int print_int(va_list types, char buffer[], int flags, int width, int precision, int size)
+int print_int(va_list types, char buffer[], int flags, 
+		int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
 	int is_negative = 0;
@@ -148,7 +152,8 @@ int print_int(va_list types, char buffer[], int flags, int width, int precision,
  * @size: Size specifier
  * Return: Numbers of char printed.
  */
-int print_binary(va_list types, char buffer[], int flags, int width, int precision, int size)
+int print_binary(va_list types, char buffer[], int flags, 
+		int width, int precision, int size)
 {
 	unsigned int n, m, i, sum;
 	unsigned int a[32];
